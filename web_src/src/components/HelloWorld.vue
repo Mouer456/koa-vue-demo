57f02fe240c5133858c3793e4190c153a60d07b1
wsq <3053878970@qq.com>
2020-01-07 21:16:42 +0800

    vuejs

--- web_src/src/components/HelloWorld.vue
+++ web_src/src/components/HelloWorld.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="hello">
-    <h3>{{ msg }}</h3>
-    <div>koa2接口测试：</div>
-    <div>{{ dataTest }}</div>
+    <van-tabs v-model="active" scrollspy sticky>
+      <van-tab title="标签 1">
+        <p>标签 1：</p>
+        <p />
+        {{ msg }}
+      </van-tab>
+      <van-tab title="标签 2">
+        <p>标签 2：</p>
+        <div>koa2接口测试：</div>
+        {{ koaData }}
+      </van-tab>
+      <van-tab title="标签 3">
+        <p>标签 3：Calendar组件</p>
+        <van-cell title="日期" :value="date" @click="show = true" is-link />
+        <van-calendar v-model="show" @confirm="onConfirm" />
+      </van-tab>
+      <van-tab title="标签 4">
+        <p>标签 4：</p>
+        内容 4
+      </van-tab>
+      <van-tab title="标签 5">
+        <p>标签 5：</p>
+        内容 5
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import { Tab, Tabs, Cell, Calendar } from 'vant';
+
+Vue.use(Tab)
+  .use(Tabs)
+  .use(Cell)
+  .use(Calendar);
+
 export default {
   name: 'HelloWorld',
   props: {
@@ -14,7 +44,10 @@ export default {
   },
   data() {
     return {
-      dataTest: {}
+      active: 0,
+      koaData: {},
+      date: '',
+      show: false
     };
   },
   mounted() {
@@ -22,10 +55,20 @@ export default {
   },
   methods: {
     apiTest() {
-      this.$axios.get('/api/user/userinfo').then(res => {
-        console.log(res);
-        this.dataTest = res;
+      this.$axios.get('/api/user/userAllInfo_sqlite').then(res => {
+        // console.log(res);
+        this.koaData = res;
       });
+    },
+    formatDate(date) {
+      return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    },
+    onConfirm(date) {
+      this.show = false;
+
+      this.date = this.formatDate(date);
+      console.log(this.date);
+      console.log(date);
     }
   }
 };
--- web_src/vue.config.js
+++ web_src/vue.config.js
@@ -1,5 +1,24 @@
+const flagProduction = process.env.VUE_APP_FLAG === 'production';
+const flagTest = process.env.VUE_APP_FLAG === 'test';
+
 module.exports = {
   publicPath: './',
   outputDir: '../web',
-  productionSourceMap: false // 生产环境去掉map文件
+  productionSourceMap: !flagProduction, // 生产环境去掉map文件
+
+  chainWebpack: config => {
+    if (flagTest) {
+      // 命令: npm run test 测试环境 + 包分析
+      config
+        .plugin('webpack-bundle-analyzer')
+        .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+        .end();
+    }
+  },
+  configureWebpack: {
+    optimization: {
+      // 根据需要为各应用分离代码块
+      splitChunks: {}
+    }
+  }
 };
